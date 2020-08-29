c9570e3def960ce5c0c293dca148c546fb598a42
awesomeayaan13 <41842116+awesomeayaan13@users.noreply.github.com>
2020-08-28 21:46:32 -0400

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,21 @@
+class Ground{
+    constructor(x,y,w,h){
+        var options={
+            isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,w,h,options)
+        this.width=w;
+        this.height=h;
+        World.add(world,this.body)  
+    }
+    display(){
+        var pos=this.body.position;
+        rectMode(CENTER);
+        fill(41, 216, 240);
+        rect(pos.x,pos.y,this.width,this.height);
+
+        
+    }
+
+    
+}
